73b680ffa6c2d01b12b0143d2d542858d08aa32e
matheushent <matheustosta66@gmail.com>
2020-04-08 23:10:08 -0300

    image related operations up

--- utils/image.py
+++ utils/image.py
@@ -5,19 +5,16 @@ def resize_activations(enhanced_model_output, input_shape):
     """Utility function to resize a given tensor
 
     Args:
-        enhanced_model_output:
-        input_shape (tuple): shape of the input, e.g. (224, 224)
+        enhanced_model_output (tf.Tensor): 4D-Tensor with shape (batch_size, H, W, K)
+        input_shape (Tuple[int, int]): shape of the input, e.g. (224, 224)
 
     Returns
-        tensor (tf.Tensor): 4D-Tensor with shape (enhanced_model_output.shape[-1], H, W, K)
+        tensor (tf.Tensor): 4D-Tensor with shape (batch_size, H, W, K)
     """
-    resized_activations = []
-    for i in range(enhanced_model_output.shape[-1]):
 
-        # resizing every activation map to original input image spatial dimensions
-        resized_activations.append(tf.image.resize(enhanced_model_output[..., i], input_shape, preserve_aspect_ratio=True))
+    image = tf.image.resize(enhanced_model_output, input_shape, preserve_aspect_ratio=True)
 
-    return tf.stack(resized_activations)
+    return image
 
 def normalize_activations(tensor):
     """Utility function to normalize a given tensor
@@ -30,7 +27,7 @@ def normalize_activations(tensor):
     """
     
     normalized_tensor = tf.cast(
-        tf.image.per_image_standardization(tensor), tf.uint8
+        tf.image.per_image_standardization(tensor), tf.float32
     )
 
     return normalized_tensor
