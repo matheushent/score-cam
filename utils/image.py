22e190998e4852ba7882e1e790b569a62d3dde46
matheushent <matheustosta66@gmail.com>
2020-06-15 18:53:22 -0400

    change functions to fix bugs

--- utils/image.py
+++ utils/image.py
@@ -1,39 +1,57 @@
 """Module for image related operations"""
+from skimage import transform
 import tensorflow as tf
+import numpy as np
 
-def resize_activations(enhanced_model_output, input_shape):
+def resize_activations(tensor, input_shape):
     """Utility function to resize a given tensor
 
     Args:
-        enhanced_model_output (tf.Tensor): 4D-Tensor with shape (batch_size, H, W, K)
+        tensor (tf.Tensor): 4D-Tensor with shape (batch_size, H, W, K)
         input_shape (Tuple[int, int]): shape of the input, e.g. (224, 224)
 
     Returns
-        tensor (tf.Tensor): 4D-Tensor with shape (batch_size, H, W, K)
+        tensor (tf.Tensor): 4D-Tensor with shape (batch_size, K, H, W)
     """
 
-    image = tf.image.resize(enhanced_model_output, input_shape, preserve_aspect_ratio=True)
+    resized_activation = list()
 
-    return image
+    for j in range(tensor.shape[0]):
+
+        activations = list()
+
+        for i in range(tensor.shape[-1]):
+            activations.append(
+                transform.resize(tensor[j, ..., i], input_shape, preserve_range=True)
+            )
+        
+        resize_activations.append(np.array(activations))
+
+    return tf.convert_to_tensor(np.array(resize_activations), dtype=tf.float32)
 
 def normalize_activations(tensor):
     """Utility function to normalize a given tensor
 
     Args:
-        tensor (tf.Tensor): 4D-Tensor with shape (batch_size, H, W, K)
+        tensor (tf.Tensor): 4D-Tensor with shape (batch_size, K, H, W)
     
     Returns
-        tf.Tensor: 4D-Tensor with shape (batch_size, H, W, K) μ = 0 and σ = 1
+        tf.Tensor: 4D-Tensor with shape (batch_size, K, H, W)
     """
 
-    normalized_tensor = tf.linalg.normalize(
-        tensor, ord='euclidean', axis=None
-    )[0]
+    tensors = list()
+
+    # goes through each image
+    for i in range(tensor.shape[0]):
+        flattened = tf.reshape(tensor[i], (tensor[i].shape[0], -1))
+
+        max_a = tf.math.reduce_max(flattened, axis=1)
+        min_a = tf.math.reduce_min(flattened, axis=1)
+
+        diffs = tf.where(max_a > min_a, max_a - min_a, 1)
+
+        normalized_tensor = (tensor[i] - tf.reshape(min_a, (-1, 1, 1))) / tf.reshape(diffs, (-1, 1, 1))
+
+        tensors.append(normalized_tensor)
     
-    # ensure dtype == tf.float32
-    if normalized_tensor.dtype != tf.float32:
-        normalized_tensor = tf.cast(
-            normalized_tensor, tf.float32
-        )
-    
-    return normalized_tensor
+    return tf.stack(tensors, axis=0)
